899e938f0532cea85f4d9b6e0618a63615ed3771
Ricardo Pietrobon <rpietro@duke.edu>
2012-06-14 17:06:36 -0400

    first rough draft

--- template_secondary_data_analysis.R
+++ template_secondary_data_analysis.R
@@ -24,7 +24,7 @@ library("car")
 #IMPORTING DATA AND RECODING
 #####################################################################################
 
-#if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object
+#if you are using a file that is local to your computer, then replace path below by path to the data file. command will throw all the data into the templateData object. replace the word template.data by a name that might easier for you to remember and that represents your data
 templateData <- read.csv("/Users/rpietro/Google Drive/R/nonpublicdata/data.csv")
 
 
@@ -36,94 +36,41 @@ templateData <- read.csv("/Users/rpietro/Google Drive/R/nonpublicdata/data.csv")
 str(templateData)
 #list variable names so that they can be used later
 names(templateData)
+#below will attach your data so that when you execute a command you don't have to write the name of your data object over and over again
 attach(templateData)
 
 #function below is used to recode variables. things to notice: replace old.var with the variable you are recoding, replace new.var with the variable you want to create. the whole recoding happens within " ". all character and factor variables will be within '', numbers will be displayed with digits (not inside '') or NA (also without '')
-new.var  <- recode(old.var, " c(1,2) = 'A'; else = 'B' ")
+new.var  <- car::recode(old.var, " c(1,2) = 'A'; else = 'B' ")
 
 ###########################################################################################
 #TABLE 1: DEMOGRAPHICS
 ###########################################################################################
-
+#describes your entire dataset
 describe(templateData)
 
-t.test(age~year)
-t.test(bmi~year)
-t.test(hgb~year)
-t.test(creatinine~year)
-#below doesn't work since the missing values for wbc overlap with the values where year = 0, leaving year with a single category
-t.test(wbc~year)
+summary(variable)
+qplot(variable)
 
-#below is a function for testing multiple associations at the same time
-# ttest.outcomes  <- function(integer.vector, predictor) 
-#   {
-#   data.frame(integer.vector, predictor) #equalizing vector length
-#   for (i in integer.vector) 
-#     t.test(i ~ predictor, na.action=na.exclude)
-# }
+#t.test, where outcome is a continuous variable and predictor is a dichotomous variable
+t.test(outcome~predictor)
 
-CrossTable(year, female, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, asa2groups, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, difinal, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, operation, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, op2groups, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, laparoscopic, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
-CrossTable(year, rac3groups, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
+#chi square test where both outcome and predictor are categorical variables
+CrossTable(outcome, predictor, chisq=TRUE, missing.include=TRUE, format="SAS", prop.r=FALSE)
 
-summary(bmi)
-qplot(bmi)
-
-
-kruskal.test(creatinine ~ year)
-summary(creatinine~year)
 
 ########################################################################################
-# TABLE 2
+# Table with models
 ########################################################################################
 
-#####################################################################################
-#TABLE 3
-#####################################################################################
-
-model1  <- glm(atleastonecompl~year,family=binomial(link="logit"))
-summary(model1) #gives you model results
-coefficients(model1) # model coefficients
-confint(model1, level=0.95) # CIs for model parameters 
-fitted(model1) # predicted values
-residuals(model1) # residuals
-anova(model1) # anova table, something like anova(model1, model2) will compare two nested models
-vcov(model1) # covariance matrix for model parameters 
-influence(model1) # regression diagnostics
+logisticmodel  <- glm(outcome ~ predictor + confounder,family=binomial(link="logit"))
+summary(logisticmodel) #gives you model results
+coefficients(logisticmodel) # model coefficients
+confint(logisticmodel, level=0.95) # CIs for model parameters 
+fitted(logisticmodel) # predicted values
+residuals(logisticmodel) # residuals
+influence(logisticmodel) # regression diagnostics
 layout(matrix(c(1,2,3,4),2,2)) # creates the white space for 4 graphs/page 
-plot(model1) #generates 4 graphs/page
-
-summary(model1)
-anova(model1)
-confint(model1)
-
-model2 <- glm(atleastonecompl ~ year + proclap + age + female + bmi + asa + operation + dg4gr, family=binomial(link="logit"))
-
-str((atleastonecompl, year))# + proclap + age + female + bmi + asa + operation + dg4gr)
-
-#Worni, this is new stata syntax and then i don't know what it means. if you shoot me an explanation i can give you the R equivalent
-#Pietrobon, the xi: means that it takes categorical variables as categorical and not as continuous variables
-# e.g. i.operation then means that it takes operation1 operation2 operation3 operation4 as categorical varialbes
-# if you don't add the xi: .... i.*** then it would just run operation as a continuous variable, most likely, 
-# in earlier times you had to recode to operation1 operation2 operation3 as categorical variables
-### the i.epidural*year means, that I added the interaction term between i.epidural (most likely the i.*** would not be necessary) and year 
-
-wilcox.test(los, year, conf.int = TRUE)
-
-model2  <- ln_los ~ year + laparoscopic + age + female + bmi + asa + op1 + op2 + op3 + op4 + op5 + op6 + dg1 + dg2 + dg3 + dg4 #this is your regular model
-year.pred  <- c(1,2) #these are the values you want to predict for
-model2_hat  <- predict(model1, newdata=year.pred,interval="p", level=0.95)) #here you are using your original model to predict values as a function of specific years as you specified under year.pred
-model2_hat #this command will show you the values
-
-############################################################################
-#TABLE 4
-############################################################################
-# nbreg restart_antibiotic year
-#Worni, we went over this in a workshop, remember? http://goo.gl/5BMl1
+plot(logisticmodel) #generates 4 graphs/page
 
 #######################################################################################
 #template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
