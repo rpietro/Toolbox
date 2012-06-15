ac8251c9d9eca119f76f6a4410fc08dc7f203140
Ricardo Pietrobon <rpietro@duke.edu>
2012-06-15 07:56:42 -0400

    added survival

--- template_secondary_data_analysis.R
+++ template_secondary_data_analysis.R
@@ -16,6 +16,7 @@ detach()
 
 #command below will install individual and is only run once. remove the hash tag if this is the first time you are running the code on RStudio, and then you can add the hash tag again
 install.packages("car", repos="http://cran.r-project.org")
+install.packages("ggplot2", repos="http://cran.r-project.org")
 
 #command below will install each package. if you run this script from the beginning you need to run every single one again
 library("ggplot2")
@@ -60,18 +61,26 @@ CrossTable(outcome, predictor, chisq=TRUE, missing.include=TRUE, format="SAS", p
 
 
 ########################################################################################
-# Table with models
+# TABLE WITH MODELS
 ########################################################################################
 
-logisticmodel  <- glm(outcome ~ predictor + confounder,family=binomial(link="logit"))
-summary(logisticmodel) #gives you model results
-coefficients(logisticmodel) # model coefficients
-confint(logisticmodel, level=0.95) # CIs for model parameters 
-fitted(logisticmodel) # predicted values
-residuals(logisticmodel) # residuals
-influence(logisticmodel) # regression diagnostics
+logisticmodel1  <- glm(outcome ~ predictor + confounder,family=binomial(link="logit"))
+summary(logisticmodel1) #gives you model results
+coefficients(logisticmodel1) # model coefficients
+confint(logisticmodel1, level=0.95) # CIs for model parameters 
+fitted(logisticmodel1) # predicted values
+residuals(logisticmodel1) # residuals
+influence(logisticmodel1) # regression diagnostics
 layout(matrix(c(1,2,3,4),2,2)) # creates the white space for 4 graphs/page 
-plot(logisticmodel) #generates 4 graphs/page
+plot(logisticmodel1) #generates 4 graphs/page
+
+survivalmodel1 <- coxph(Surv(time_to_event, event_yes_no) ~ predictor + confounder1 + confounder2, ties="efron")
+#below will test for proportional hazards assumption
+prop.assump1 <- cox.zph(survivalmodel1) 
+print(prop.assump1) #display results for assumption 
+plot(prop.assump1)  #plot curves -- from the help page: "If the proportional hazards assumption is true, beta(t) will be a horizontal line. The printout gives a test for slope=0."
+#summary results for the model
+summary(survivalmodel1)
 
 #######################################################################################
 #template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
